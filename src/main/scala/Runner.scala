ffa8fe1b6215fb4230edf72e68c28fbc6731eb88
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-03-10 10:39:41 +0100

    Execution

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -5,10 +5,12 @@ import com.raphtory.core.deploy.Raphtory
 import com.raphtory.output.FileOutputFormat
 
 object Runner extends App {
+  val lastTimestamp: Long = 46417964
+
   val source = ResourceSpout("part-00000-hateful_gab.csv")
   val builder = new FirstAnalysisGB()
   val graph = Raphtory.createGraph(spout = source, graphBuilder = builder)
   val output = FileOutputFormat("/home/rodrigo/output-5.0")
-  val queryHandler = graph.pointQuery(GraphDepthIndex(), output, 39316)
+  val queryHandler = graph.pointQuery(GraphDepthIndex(), output, lastTimestamp)
   queryHandler.waitForJob()
 }
