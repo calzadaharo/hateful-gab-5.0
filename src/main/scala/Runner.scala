45780367a2ec4b3409efd2704c97057e8329cca8
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-03-31 15:25:37 +0200

    Testing

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -8,12 +8,12 @@ object Runner extends App {
   val firstTest = 39316
   val lastTimestampPart0: Long = 3062658
   val lastTimestamp: Long = 46417964
-  val maxIndex = 2698
+  val maxIndex = 2692
 
-  val source = ResourceSpout("pre-virality-2-9.csv")
+  val source = ResourceSpout("pre-virality.csv")
   val builder = new ViralityAnalysisGB()
   val graph = Raphtory.createGraph(spout = source, graphBuilder = builder)
   val output = FileOutputFormat("/home/rodrigo/output-5.0")
   val outputServer = FileOutputFormat("/home/rcalzada/output-5.0")
-  val queryHandler = graph.pointQuery(EfficientVirality(), outputServer, 9)
+  val queryHandler = graph.pointQuery(EfficientVirality(), outputServer, maxIndex)
 }
