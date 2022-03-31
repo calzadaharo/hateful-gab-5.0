d446b257a74e6325fc411395d12c5de9895aabc8
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-03-31 10:51:40 +0200

    Testing

--- src/main/scala/HGGraphBuilders/ViralityAnalysisGB.scala
+++ src/main/scala/HGGraphBuilders/ViralityAnalysisGB.scala
@@ -7,9 +7,9 @@ class ViralityAnalysisGB extends GraphBuilder[String]{
   override def parseTuple(tuple: String): Unit = {
     val dataLine = tuple.split(",").map(_.trim)
 
-    val vertex = dataLine(0).toLong
-    val parent = dataLine(1)
-    val cascade = dataLine(2)
+    val vertex = dataLine(1).toLong
+    val parent = dataLine(2)
+    val cascade = dataLine(0)
     val timestamp = dataLine(3)
     val index = dataLine(4).toLong
     val level = dataLine(5)
